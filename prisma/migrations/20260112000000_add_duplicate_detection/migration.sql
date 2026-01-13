79bf7b496b320a71337e71d1e0a92baffdc77e9e
Jared Trent <jaredtrentm@gmail.com>
2026-01-12 23:31:34 -0600

    fix: Use db push instead of migrations for initial database setup

--- prisma/migrations/20260112000000_add_duplicate_detection/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "Transaction" ADD COLUMN "isDuplicateFlag" BOOLEAN NOT NULL DEFAULT false;
-ALTER TABLE "Transaction" ADD COLUMN "duplicateOfId" TEXT;
-ALTER TABLE "Transaction" ADD COLUMN "duplicateScore" INTEGER;
